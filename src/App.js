e911c55622b44a2712fab9aa029cdc485fb7ef55
Shafak93 <shafakctg64@gmail.com>
2022-07-28 13:02:42 +0600

    ReceiptForm compo create and bootstrap install

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ReceiptForm from './Components/Receipt/ReceiptForm';
 
 function App() {
   return (
     <div className="App">
-      <p>Git test</p>
+    <ReceiptForm></ReceiptForm>
     </div>
   );
 }
