b3c7c6a07e922130112f21ad95af7a4fb787bf26
Shafak93 <shafakctg64@gmail.com>
2022-07-28 15:20:39 +0600

    form design and functionality added

--- src/Components/Receipt/ReceiptForm.js
+++ src/Components/Receipt/ReceiptForm.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 
 const ReceiptForm = () => {
+    const [items, setItems] = useState([])
+    const handleAddItem = (event) =>{
+        event.preventDefault();
+        const date = event.target.date.value;
+        const amount = event.target.amount.value;
+        const payment = event.target.payment.value;
+        const remark = event.target.remark.value;
+        const allItems = [{date,amount, payment, remark}]
+        setItems(...allItems, allItems)
+        console.log(items);
+    }
     return (
-        <div className='container w-50'>
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
+        <div className='container w-50 mt-5'>
+            <Form onSubmit={handleAddItem}>
+                <Form.Group className="mb-3 d-flex justify-content-between align-items-center" controlId="formBasicDate">
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control className='w-75' name='date' type="date" placeholder="Enter date" />
+                </Form.Group>
+                <Form.Group className="mb-3 d-flex justify-content-between align-items-center" controlId="formBasicAmount">
+                    <Form.Label>Amount</Form.Label>
+                    <Form.Control className='w-75' name='amount' type="text" placeholder="Enter Amount (in INR)" />
+                </Form.Group>
+                <Form.Group className="mb-3 d-flex justify-content-between align-items-center" controlId="formBasicPayment">
+                    <Form.Label>Payment Mode</Form.Label>
+                    <select name='payment' class="w-75" id="inlineFormCustomSelect">
+                        <option selected>Choose...</option>
+                        <option value="cash">Cash</option>
+                        <option value="cheque">Cheque</option>
+                        
+                    </select>
+                </Form.Group>
+                <Form.Group className="mb-3 d-flex justify-content-between align-items-center" controlId="formBasicRemark">
+                    <Form.Label>Remark</Form.Label>
+                    <Form.Control name='remark' className='w-75' type="taxt" placeholder="Enter Remark" />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
+                
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
             </Form>
+
+            
         </div>
     );
 };
